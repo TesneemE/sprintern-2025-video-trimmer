adbc620da286bd7121cd6417ae26aff013777e12
TesneemE <116093093+TesneemE@users.noreply.github.com>
2025-03-02 01:22:58 -0500

    Update _app.js

--- video_clipper_app/pages/_app.js
+++ video_clipper_app/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import "@/styles/home.module.css";
+import "@/styles/Home.module.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
